085d2eaab81c6a4bf543a685a42b873bc757ebb2
lolimilkita <caturcno.cr@gmail.com>
2022-11-10 13:41:05 +0700

    implement validation using joi

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,3 +1,4 @@
+import { createProductValidation } from '../validation/product.validation'
 import { logger } from '../utils/logger'
 import { Router, Request, Response, NextFunction } from 'express'
 
@@ -5,9 +6,16 @@ export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('Success get product data')
-  res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 200000 }] })
+  return res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 200000 }] })
 })
 ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = createProductValidation(req.body)
+
+  if (error) {
+    logger.error('ERR: product - create', error.details[0].message)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
+  }
+
   logger.info('Success add new product')
-  res.status(200).send({ status: true, statusCode: 200, data: req.body })
+  return res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
 })
