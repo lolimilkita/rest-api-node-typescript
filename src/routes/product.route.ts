e7ff0344a597b3acc2d150387b0da61ab8ea2e4a
lolimilkita <caturcno.cr@gmail.com>
2022-11-10 14:41:49 +0700

    Create & implement controller

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,21 +1,8 @@
-import { createProductValidation } from '../validation/product.validation'
-import { logger } from '../utils/logger'
-import { Router, Request, Response, NextFunction } from 'express'
+import { getProduct, createProduct } from '../controllers/product.controller'
+import { Router } from 'express'
 
 export const ProductRouter: Router = Router()
 
-ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  logger.info('Success get product data')
-  return res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 200000 }] })
-})
-ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = createProductValidation(req.body)
-
-  if (error) {
-    logger.error('ERR: product - create', error.details[0].message)
-    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
-  }
-
-  logger.info('Success add new product')
-  return res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
-})
+ProductRouter.get('/', getProduct)
+ProductRouter.get('/:name', getProduct)
+ProductRouter.post('/', createProduct)
